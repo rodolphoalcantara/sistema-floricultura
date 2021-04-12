65e96f23c17a1a646213f21ed4f31bd766f5a269
Rodolpho Alcântara <rodolphoalcantara@outlook.com>
2021-04-12 12:26:33 -0300

    Adição das classes do projeto

---
+++ sistema-floricultura/src/main/java/br/com/floricultura/sistema/model/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.floricultura.sistema.model;
+
+public class Cliente {
+    private int id;
+    private String cpf;
+    private String nome;
+    private String email;
+    private String estadoCivil;
+    private String dataNascimento;
+       
+}
---
+++ sistema-floricultura/src/main/java/br/com/floricultura/sistema/model/Produto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.floricultura.sistema.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Rodolpho Alcântara
+ */
+public class Produto {
+    private int id;
+    private String nome;
+    private Double valor;
+    private int estoque;
+    private String tipo;
+    private String descricao;
+}
