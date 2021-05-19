987f442aa6bc05a0f2f9c5a5c848bbcb43afc23e
Gessione Freitas de Souza <gesouza@brq.com>
2021-05-18 23:47:07 -0300

    ADD DAOProduto, ATT SQL, Final Cadastrar produto

--- bd-resources/criacao-db-tables.sql
+++ bd-resources/criacao-db-tables.sql
@@ -2,63 +2,55 @@ create database sistema_floricultura;
 use sistema_floricultura;
 
 create table endereco(
-	id_end int not null auto_increment,
+    id_end int not null auto_increment,
     estado varchar(40) not null,
     cudade varchar(40) not null,
     bairro varchar(40) not null,
     rua varchar(120) not null,
-    
+
     primary key (id_end)
 );
 
 create table cliente(
-	id_cli int not null auto_increment,
+    id_cli int not null auto_increment,
     CPF varchar(11) not null unique,
     nome varchar(60) not null,
     email varchar(120) not null,
     estado_civil varchar(30) not null,
     data_nasc date not null,
     fk_id_endereco int not null,
-    
+
     primary key (id_cli),
     foreign key (fk_id_endereco) references endereco(id_end)
 );
 
 create table venda(
-	id_venda int not null auto_increment,
+    id_venda int not null auto_increment,
     data_venda date not null,
     fk_id_cli int not null,
-    
+
     primary key (id_venda),
     foreign key (fk_id_cli) references cliente(id_cli)
 );
 
-create table tipo_prod(
-	id_tipo int not null auto_increment,
-    nome varchar(30) not null,
-    
-    primary key (id_tipo)
-);
-
 create table produto(
-	id_produto int not null auto_increment,
+    id_produto int not null auto_increment,
     nome varchar(60) not null,
     valor_unitario decimal(10,2) not null,
     estoque int not null,
     descricao varchar(500),
-    fk_id_tipo_prod int not null,
-    
-    primary key (id_produto),
-    foreign key (fk_id_tipo_prod) references tipo_prod(id_tipo)
+    tipo varchar(30) not null,
+
+    primary key (id_produto)
 );
 
 create table item_venda(
-	id_item int not null auto_increment,
+    id_item int not null auto_increment,
     quantidade int not null,
     valor_total decimal(10,2) not null,
     fk_id_venda int not null,
     fk_id_produto int not null,
-    
+
     primary key (id_item),
     foreign key (fk_id_venda) references venda(id_venda),
     foreign key (fk_id_produto) references produto(id_produto)
--- sistema-floricultura/src/main/java/br/com/floricultura/sistema/model/Produto.java
+++ sistema-floricultura/src/main/java/br/com/floricultura/sistema/model/Produto.java
@@ -18,5 +18,62 @@ public class Produto {
     private String descricao;
     
     
+    public Produto(){
+    
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Computador{" + "id=" + id + ", nome=" + nome + ", valor=" + valor + ", estoque=" + estoque + ", tipo=" + tipo + ", descricao=" + descricao + '}';
+    }
     
 }
