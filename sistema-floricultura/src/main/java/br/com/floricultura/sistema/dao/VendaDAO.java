9687d3f517548a241079ca1b141370dac73b103f
Rodolpho Alcântara <rodolphoalcantara@outlook.com>
2021-05-23 21:45:50 -0300

    Alteracao na DAO de vendas

--- sistema-floricultura/src/main/java/br/com/floricultura/sistema/dao/VendaDAO.java
+++ sistema-floricultura/src/main/java/br/com/floricultura/sistema/dao/VendaDAO.java
@@ -36,9 +36,138 @@ public class VendaDAO {
                     + "from venda v "
                     + "inner join cliente c on c.id_cli = v.fk_id_cli "
                     + "inner join item_venda i on v.id_venda = i.fk_id_venda "
-                    + "inner join produto p on p.id_produto = i.fk_id_produto";
+                    + "inner join produto p on p.id_produto = i.fk_id_produto "
+                    + "order by v.data_venda desc";
             
             try(PreparedStatement pstm = GerenciadorConexao.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
+                pstm.execute();
+                
+                try(ResultSet rst = pstm.getResultSet()){
+                    while(rst.next()){
+                        if(anterior == null || anterior.getId() == rst.getInt(1)){
+                            CadastroCliente cliente = new CadastroCliente(rst.getInt(3), rst.getString(4), rst.getString(5), rst.getString(6),rst.getString(7), rst.getString(8), rst.getString(9), rst.getString(10), rst.getInt(11));
+                            Venda venda = new Venda(cliente, rst.getDate(2));
+                            vendas.add(venda);
+                            anterior = venda;
+                        }
+                        Produto produto = new Produto(rst.getInt(15), rst.getString(16), rst.getDouble(17), rst.getInt(18), rst.getString(19), rst.getString(20));
+                        ItemVenda item = new ItemVenda(rst.getInt(12), rst.getInt(13),rst.getBigDecimal(14), produto, rst.getInt(1));
+                        anterior.adicionar(item);
+                    }
+                }
+                return vendas;
+            }
+            
+        }catch(SQLException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    public static List<Venda> listarVendasComItensEmUmPeriodo(Date dataInicial, Date dataFinal) throws ClassNotFoundException{
+        try{
+            Venda anterior = null;
+            List<Venda> vendas = new ArrayList<Venda>();
+            String query = "select v.id_venda, v.data_venda, "
+                    + "c.id_cli, c.nome, c.CPF, c.sexo, c.data_nasc, c.email, c.estado_civil, c.telefone, c.fk_id_endereco "
+                    + "i.id_item, i.quantidade, i.valor_total, "
+                    + "p.id_produto, p.nome, p.valor_unitario, p.estoque, p.descricao, p.tipo "
+                    + "from venda v "
+                    + "inner join cliente c on c.id_cli = v.fk_id_cli "
+                    + "inner join item_venda i on v.id_venda = i.fk_id_venda "
+                    + "inner join produto p on p.id_produto = i.fk_id_produto "
+                    + "where v.data_venda between ? and ? "
+                    + "order by v.data_venda desc ";
+            
+            try(PreparedStatement pstm = GerenciadorConexao.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
+                
+                pstm.setDate(1, dataInicial);
+                pstm.setDate(2, dataFinal);
+                
+                pstm.execute();
+                
+                try(ResultSet rst = pstm.getResultSet()){
+                    while(rst.next()){
+                        if(anterior == null || anterior.getId() == rst.getInt(1)){
+                            CadastroCliente cliente = new CadastroCliente(rst.getInt(3), rst.getString(4), rst.getString(5), rst.getString(6),rst.getString(7), rst.getString(8), rst.getString(9), rst.getString(10), rst.getInt(11));
+                            Venda venda = new Venda(cliente, rst.getDate(2));
+                            vendas.add(venda);
+                            anterior = venda;
+                        }
+                        Produto produto = new Produto(rst.getInt(15), rst.getString(16), rst.getDouble(17), rst.getInt(18), rst.getString(19), rst.getString(20));
+                        ItemVenda item = new ItemVenda(rst.getInt(12), rst.getInt(13),rst.getBigDecimal(14), produto, rst.getInt(1));
+                        anterior.adicionar(item);
+                    }
+                }
+                return vendas;
+            }
+            
+        }catch(SQLException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    public static List<Venda> listarVendasComItensEmUmPeriodoPorCliente(Date dataInicial, Date dataFinal, int idCliente) throws ClassNotFoundException{
+        try{
+            Venda anterior = null;
+            List<Venda> vendas = new ArrayList<Venda>();
+            String query = "select v.id_venda, v.data_venda, "
+                    + "c.id_cli, c.nome, c.CPF, c.sexo, c.data_nasc, c.email, c.estado_civil, c.telefone, c.fk_id_endereco "
+                    + "i.id_item, i.quantidade, i.valor_total, "
+                    + "p.id_produto, p.nome, p.valor_unitario, p.estoque, p.descricao, p.tipo "
+                    + "from venda v "
+                    + "inner join cliente c on c.id_cli = v.fk_id_cli "
+                    + "inner join item_venda i on v.id_venda = i.fk_id_venda "
+                    + "inner join produto p on p.id_produto = i.fk_id_produto "
+                    + "where c.id_cli like ? and v.data_venda between ? and ? "
+                    + "order by v.data_venda desc ";
+            
+            try(PreparedStatement pstm = GerenciadorConexao.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
+                
+                pstm.setDate(2, dataInicial);
+                pstm.setDate(3, dataFinal);
+                
+                pstm.execute();
+                
+                try(ResultSet rst = pstm.getResultSet()){
+                    while(rst.next()){
+                        if(anterior == null || anterior.getId() == rst.getInt(1)){
+                            CadastroCliente cliente = new CadastroCliente(rst.getInt(3), rst.getString(4), rst.getString(5), rst.getString(6),rst.getString(7), rst.getString(8), rst.getString(9), rst.getString(10), rst.getInt(11));
+                            Venda venda = new Venda(cliente, rst.getDate(2));
+                            vendas.add(venda);
+                            anterior = venda;
+                        }
+                        Produto produto = new Produto(rst.getInt(15), rst.getString(16), rst.getDouble(17), rst.getInt(18), rst.getString(19), rst.getString(20));
+                        ItemVenda item = new ItemVenda(rst.getInt(12), rst.getInt(13),rst.getBigDecimal(14), produto, rst.getInt(1));
+                        anterior.adicionar(item);
+                    }
+                }
+                return vendas;
+            }
+            
+        }catch(SQLException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    public static List<Venda> listarVendasComItensPorCliente(int idCliente) throws ClassNotFoundException{
+        try{
+            Venda anterior = null;
+            List<Venda> vendas = new ArrayList<Venda>();
+            String query = "select v.id_venda, v.data_venda, "
+                    + "c.id_cli, c.nome, c.CPF, c.sexo, c.data_nasc, c.email, c.estado_civil, c.telefone, c.fk_id_endereco "
+                    + "i.id_item, i.quantidade, i.valor_total, "
+                    + "p.id_produto, p.nome, p.valor_unitario, p.estoque, p.descricao, p.tipo "
+                    + "from venda v "
+                    + "inner join cliente c on c.id_cli = v.fk_id_cli "
+                    + "inner join item_venda i on v.id_venda = i.fk_id_venda "
+                    + "inner join produto p on p.id_produto = i.fk_id_produto "
+                    + "where c.id_cli like ?"
+                    + "order by v.data_venda desc ";
+            
+            try(PreparedStatement pstm = GerenciadorConexao.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
+                
+                pstm.setInt(1, idCliente);
+                
                 pstm.execute();
                 
                 try(ResultSet rst = pstm.getResultSet()){
